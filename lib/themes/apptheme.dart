613cb5eb6c306bea5f9a8fe0e2f409ad9095856f
duranysd <duranysd1119@gmail.com>
2024-11-18 22:48:54 -0600

    DarkMode

--- lib/themes/apptheme.dart
+++ lib/themes/apptheme.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color.fromARGB(255, 137, 145, 255); 
-  static const Color primaryVariant = Color.fromARGB(255, 86, 53, 163);
-  static const Color secondary = Color(0xFF03DAC6); 
-  static const Color background = Color(0xFFF5F5F5); 
+  // Colores personalizados
+  static const Color primary = Color.fromARGB(255, 137, 145, 255);
+  static const Color primaryVariant = Color(0xFF3700B3);
+  static const Color secondary = Color(0xFF03DAC6);
+  static const Color background = Color(0xFFF5F5F5);
   static const Color surface = Colors.white;
-  static const Color error = Color(0xFFB00020); 
+  static const Color error = Color.fromARGB(255, 187, 0, 34);
   static const Color textPrimary = Colors.black;
   static const Color textSecondary = Colors.grey;
+  static const Color button = Color.fromARGB(255, 137, 145, 255);
 }
 
 class AppTypography {
@@ -24,7 +26,7 @@ class AppTypography {
     color: AppColors.textPrimary,
   );
 
-  static const TextStyle bodyText1 = TextStyle(
+  static const TextStyle paragraph = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.normal,
     color: AppColors.textSecondary,
@@ -37,7 +39,7 @@ class AppTypography {
   );
 }
 
-class AppTheme {
+class AppTheme { //Tema global 
   static ThemeData lightTheme = ThemeData(
     primaryColor: AppColors.primary,
     brightness: Brightness.light,
@@ -48,15 +50,23 @@ class AppTheme {
       elevation: 0,
       centerTitle: true,
     ),
-    textTheme: const TextTheme(
-      headlineLarge: AppTypography.headline1,
-      headlineMedium: AppTypography.headline2,
-      bodyLarge: AppTypography.bodyText1,
-      labelLarge: AppTypography.button,
+    iconTheme: const IconThemeData(
+      color: AppColors.primary, // El color de los iconos
+      size: 24,
     ),
-    buttonTheme: ButtonThemeData(
-      buttonColor: AppColors.primary,
-      textTheme: ButtonTextTheme.primary,
+    textTheme: const TextTheme(
+      headlineLarge: AppTypography.headline1, // Para los titulos
+      headlineMedium: AppTypography.headline2, // Para subtitulos
+      bodyLarge: AppTypography.paragraph, // Texto en general jeje
+      labelLarge: AppTypography.button, // Para el texto que este en algun boton
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: AppTypography.button,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
     ),
   );
 
@@ -64,11 +74,29 @@ class AppTheme {
     primaryColor: AppColors.primary,
     brightness: Brightness.dark,
     scaffoldBackgroundColor: Colors.black,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.primaryVariant,
+      foregroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+    ),
+    iconTheme: const IconThemeData(
+      color: Colors.white,
+      size: 24,
+    ),
     textTheme: const TextTheme(
       headlineLarge: AppTypography.headline1,
       headlineMedium: AppTypography.headline2,
-      bodyLarge: AppTypography.bodyText1,
+      bodyLarge: AppTypography.paragraph,
       labelLarge: AppTypography.button,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: AppTypography.button,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
     ),
   );
 }
